954f560d972950c8ffd59804115ca346894c655a
fauzanpr <fauzanpr.06@gmail.com>
2022-04-21 23:24:14 +0700

    Final Commit Jobsheet 10 Latihan

--- siakad/resources/views/mahasiswa/detail.blade.php
+++ siakad/resources/views/mahasiswa/detail.blade.php
@@ -7,6 +7,7 @@
                 Detail Mahasiswa
             </div>
             <div class="card-body">
+                <img width="100px" class="rounded mx-auto d-block" src="{{ $Mahasiswa->image==''? asset('/storage/images/default.png'): asset('/storage/' . $Mahasiswa->image) }}">
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item"><b>Nim: </b>{{$Mahasiswa->nim}}</li>
                     <li class="list-group-item"><b>Nama: </b>{{$Mahasiswa->nama}}</li>
